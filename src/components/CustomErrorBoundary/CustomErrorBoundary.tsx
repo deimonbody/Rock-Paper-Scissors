af34e765165e521c35e7cb16583c6b2b3e4d10ff
deimonbody <pavlov.d@donnu.edu.ua>
2022-12-27 15:07:34 +0200

    Added some spaces

--- src/components/CustomErrorBoundary/CustomErrorBoundary.tsx
+++ src/components/CustomErrorBoundary/CustomErrorBoundary.tsx
@@ -5,6 +5,7 @@ import styles from "./style.module.scss";
 interface IProps {
   error: Error;
 }
+
 const CustomErrorBoundary: React.FC<IProps> = ({ error }) => (
   <div className={styles.errorWrapper}>
     <p className={styles.errorMessage}>
